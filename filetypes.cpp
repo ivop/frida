63be8395bfbeda39668ff0b8de01098292ef8c48
Ivo van Poorten <ivopvp@gmail.com>
2023-03-18 02:31:39 +0100

    better file format list

--- filetypes.cpp
+++ filetypes.cpp
@@ -2,10 +2,10 @@
 #include "frida.h"
 
 QList<struct filetype> filetypes = {
-{ "Raw",                        FT_RAW_FILE },
-{ "Atari 8-bit Binary (.XEX)",  FT_ATARI8BIT_BINARY },
-{ "C64 Binary (.PRG)",          FT_C64_BINARY },
-{ "C64 PSID/RSID (.SID)",       FT_C64_PSID },
-{ "Atari 2600 2K/4K ROM (.A26)", FT_ATARI2600_2K4K },
-{ "Oric Tape File (.TAP)",      FT_ORIC_TAP },
+{ "Raw",                                FT_RAW_FILE },
+{ "Atari 8-bit Binary (.XEX)",          FT_ATARI8BIT_BINARY },
+{ "Commodore C64 Binary (.PRG)",        FT_C64_BINARY },
+{ "Commodore C64 PSID/RSID (.SID)",     FT_C64_PSID },
+{ "Atari 2600 2K/4K ROM (.A26)",        FT_ATARI2600_2K4K },
+{ "Oric Tape File (.TAP)",              FT_ORIC_TAP },
 };
