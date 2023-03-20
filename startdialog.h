7b49f3df33693a6dcde064e647c1bc8d4e4941d5
Ivo van Poorten <ivopvp@gmail.com>
2023-03-20 01:36:28 +0100

    tag everything with GPL2

--- startdialog.h
+++ startdialog.h
@@ -1,3 +1,25 @@
+// ---------------------------------------------------------------------------
+//
+// This file is part of:
+//
+// FRIDA - FRee Interactive DisAssembler
+// Copyright (C) 2017,2023 Ivo van Poorten
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; ONLY version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// ---------------------------------------------------------------------------
+
 #ifndef STARTDIALOG_H
 #define STARTDIALOG_H
 
