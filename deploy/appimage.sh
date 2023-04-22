fc70f8c3c5bd7ae6e9d7977abeabea1d3c8a0fa0
Ivo van Poorten <ivopvp@gmail.com>
2023-04-23 01:10:29 +0200

    there's devtoolset-11 for CentOS 7

--- deploy/appimage.sh
+++ deploy/appimage.sh
@@ -1,11 +1,11 @@
 #
 # Build with CentOS 7 and Qt 5.15.2 from qt.io (latest online installer)
 #
-# We need gcc8 for proper C++ support
+# We need proper C++ support:
 #
 # yum install centos-release-scl
-# yum install devtoolset-8-gcc devtoolset-8-gcc-c++
-# scl enable devtoolset-8 -- bash
+# yum install devtoolset-11-gcc devtoolset-11-gcc-c++
+# scl enable devtoolset-11 -- bash
 #
 # Do not forget yum -y install mesa-libGL-devel.x86_64 to satisfy -lGL.
 #
