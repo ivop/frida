9046100f873ec61715916d89c9b5b10874337c9a
Ivo van Poorten <ivopvp@gmail.com>
2023-04-22 05:39:35 +0200

    include all platform plugins and dependencies in appimage

--- appimage/commandline.sh
+++ appimage/commandline.sh
@@ -3,5 +3,6 @@ linuxdeployqt-continuous-x86_64.AppImage \
     frida                       \
     -appimage                   \
     -no-translations            \
-    -no-copy-copyright-files
+    -no-copy-copyright-files    \
+    -extra-plugins=platforms,wayland-decoration-client,wayland-graphics-integration,wayland-shell-integration,xcbglintegrations,egldeviceintegrations
 
