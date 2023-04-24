da07ec5e115a3e0808380d15170a4dd7badd0919
Ivo van Poorten <ivopvp@gmail.com>
2023-04-24 22:57:06 +0200

    use MSYS's patch to apply qt6-win8.patch

--- deploy/win64/qt6buildenv.bat
+++ deploy/win64/qt6buildenv.bat
@@ -2,5 +2,5 @@
 
 echo Setting up environment for building Qt 6.5.0
 
-set PATH=C:\Qt6\src\qtbase\bin;C:\Ninja;C:\CMake\bin;C:\Python311;C:\mingw64\bin;%PATH%
+set PATH=C:\Qt6\src\qtbase\bin;C:\Ninja;C:\CMake\bin;C:\Python311;C:\mingw64\bin;C:\msys64\usr\bin;%PATH%
 cd /D C:\Qt6\Src
--- deploy/win64/qt6devenv.bat
+++ deploy/win64/qt6devenv.bat
@@ -2,5 +2,5 @@
 
 echo Setting up environment for developing with Qt 6.5.0
 
-set PATH=C:\Qt6\bin;C:\Ninja;C:\CMake\bin;C:\Python311;C:\mingw64\bin;C:\GnuWin32\bin;%PATH%
+set PATH=C:\Qt6\bin;C:\Ninja;C:\CMake\bin;C:\Python311;C:\mingw64\bin;C:\msys64\usr\bin;C:\GnuWin32\bin;%PATH%
 cd /D C:\Users
