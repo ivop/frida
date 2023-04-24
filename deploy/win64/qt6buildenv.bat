c9cb33a8873391b53efcccb7ec6349d3a5e9964c
Ivo van Poorten <ivopvp@gmail.com>
2023-04-24 09:11:20 +0200

    add directory where built tools reside to %PATH%

--- deploy/win64/qt6buildenv.bat
+++ deploy/win64/qt6buildenv.bat
@@ -3,5 +3,5 @@
 echo Setting up environment for building Qt 6.5.0
 echo MinGW64 12.2 and GnuWin32 make
 
-set PATH=C:\Ninja;C:\CMake\bin;C:\Python311;C:\mingw64\bin;%PATH%
+set PATH=C:\Qt6\src\qtbase\bin;C:\Ninja;C:\CMake\bin;C:\Python311;C:\mingw64\bin;%PATH%
 cd /D C:\Qt6\Src
